d6c898a2b739d41d8d0048a8227a6ceb886ceaf2
Karly Nelson <karly.nelson@hey.com>
2024-10-08 12:19:49 -0500

    wip, experiment adding emojis to nav

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   content: ["_site/**/*.html"],
   safelist: [],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+    },
+  },
   plugins: [],
 };
